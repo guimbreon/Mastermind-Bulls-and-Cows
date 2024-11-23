b3200df016a5c484d5237736cd57acba1d1f63d1
Guilherme Soares <gui.acs26@gmail.com>
2024-11-23 19:02:04 +0000

    original

--- projetoPCO/src/types/Code.java
+++ projetoPCO/src/types/Code.java
@@ -1,27 +1,63 @@
 package types;
 
 import java.util.List;
+import java.util.ArrayList;
 
 //All documentation is needed.
 
 public class Code implements Cloneable{
 
 
-	public Code(List<? extends Colour> code) {
-	}
+    // Stores the list of Colours representing the code.
+    private final List<Colour> code;
 
+    /**
+     * Constructs a Code object with the given list of Colours.
+     * The list can include any subtype of Colour.
+     *
+     * @param code the list of Colours (or its subtypes) representing the code.
+     *             The list is defensively copied to ensure immutability.
+     */
+    public Code(List<? extends Colour> code) {
+//    	É PRECISO SER DEFENSIVO???
+//        if (code == null) {
+//            throw new IllegalArgumentException("Code list cannot be null.");
+//        }
+        this.code = new ArrayList<>(code);
+    }
+
+    
+    /**
+     * Returns a copy of the list of Colours representing this code.
+     *
+     * @return a new list containing the Colours of this code.
+     */
 	public List<Colour> getCode() {
-		return null;
+		return new ArrayList<>(code); 
 	}
 
 
-	public int getLength() {
-		return 0;
-	}
+    /**
+     * Returns the length of the code sequence.
+     *
+     * @return the number of Colours in the code.
+     */
+    public int getLength() {
+        return code.size();
+    }
 
 	public int[] howManyCorrect(Code other) {
 		return null; 
 	}
+	   /**
+     * Returns a textual representation of this Code in the format [c1, c2, c3, ...].
+     *
+     * @return a String representation of the Code.
+     */
+    @Override
+    public String toString() {
+        return code.toString();
+    }
 
 
 }
