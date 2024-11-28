fd220d2baf61a81ead4879766a6a1c006c324c65
vitoria <vitoriateixeiracorreia04@gmail.com>
2024-11-28 12:07:53 +0000

    bullsAndCows

--- projetoPCO/src/games/BullsAndCows.java
+++ projetoPCO/src/games/BullsAndCows.java
@@ -1,5 +1,35 @@
 package games;
 
-public class BullsAndCows {
+import types.Colour;
 
+public class BullsAndCows extends AbstractMastermindGame {
+
+	public BullsAndCows(int seed, int size, Colour[] colours) {
+		super(seed, size, colours);
+		// TODO Auto-generated constructor stub
+		}
+		
+	@Override
+	public boolean isRoundFinished() {
+		// TODO Auto-generated method stub
+		if (wasSecretRevealed() || MAX_TRIALS == getNumberOfTrials()) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public int score() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean updateScore() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	//FALTA COLOUR HINT REIMPLEMENRZ
 }
